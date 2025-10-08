0eadf56a29ca166956ee43257386e01e77183af0
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-08 05:50:48 +0300

    Create 01. Edit Element.js

---
+++ DOM Introduction/01. Edit Element.js
@@ -0,0 +1,5 @@
+function edit(element, match, replacer) {
+    const text = element.textContent;
+    const newText = text.replace(new RegExp(match, 'g'), replacer);
+    element.textContent = newText;
+}
