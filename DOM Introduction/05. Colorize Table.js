ac4b16f8f02d97f4cbaa25029b3b91ed58d84ac2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-09 05:51:20 +0300

    Create 05. Colorize Table.js

---
+++ DOM Introduction/05. Colorize Table.js
@@ -0,0 +1,8 @@
+function colorizeRows() {
+    const rows = Array.from(document.querySelectorAll('table tr'));
+    for (let i = 0; i < rows.length; i++) {
+        if (i % 2 !== 0) {
+            rows[i].style.backgroundColor = 'Teal';
+        }
+    }
+}
