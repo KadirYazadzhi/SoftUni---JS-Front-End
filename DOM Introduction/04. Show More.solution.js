db103332b944452593ceb5c25dd0655c1e25311b
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-09 05:51:18 +0300

    Create 04. Show More.solution.js

---
+++ DOM Introduction/04. Show More.solution.js
@@ -0,0 +1,7 @@
+function showTextSolution() {
+    document.getElementById('text').style.display = 'inline';
+    document.getElementById('more').style.display = 'none';
+}
+
+// This function is intended to be called when a button is clicked or similar event.
+// Example: document.getElementById('more').addEventListener('click', showTextSolution);
