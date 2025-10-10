8e6b5a88b889d580510cd0bd56e6ab03d9747d2d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-10 05:51:56 +0300

    Create 02. Time Converter.js

---
+++ Exercise - DOM Events/02. Time Converter.js
@@ -0,0 +1,34 @@
+function attachEventsListeners() {
+    const daysInput = document.getElementById('days');
+    const hoursInput = document.getElementById('hours');
+    const minutesInput = document.getElementById('minutes');
+    const secondsInput = document.getElementById('seconds');
+
+    document.getElementById('daysBtn').addEventListener('click', () => {
+        const days = Number(daysInput.value);
+        hoursInput.value = days * 24;
+        minutesInput.value = days * 1440;
+        secondsInput.value = days * 86400;
+    });
+
+    document.getElementById('hoursBtn').addEventListener('click', () => {
+        const hours = Number(hoursInput.value);
+        daysInput.value = hours / 24;
+        minutesInput.value = hours * 60;
+        secondsInput.value = hours * 3600;
+    });
+
+    document.getElementById('minutesBtn').addEventListener('click', () => {
+        const minutes = Number(minutesInput.value);
+        daysInput.value = minutes / 1440;
+        hoursInput.value = minutes / 60;
+        secondsInput.value = minutes * 60;
+    });
+
+    document.getElementById('secondsBtn').addEventListener('click', () => {
+        const seconds = Number(secondsInput.value);
+        daysInput.value = seconds / 86400;
+        hoursInput.value = seconds / 3600;
+        minutesInput.value = seconds / 60;
+    });
+}
