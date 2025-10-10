cf817ead4ff1e997b022e35bc050d012688ef76d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-10 05:51:59 +0300

    Create 03. Locked Profile.js

---
+++ Exercise - DOM Events/03. Locked Profile.js
@@ -0,0 +1,19 @@
+function lockedProfile() {
+    Array.from(document.querySelectorAll('.profile button')).forEach(button => {
+        button.addEventListener('click', (e) => {
+            const profile = e.target.parentElement;
+            const isLocked = profile.querySelector('input[type="radio"][value="lock"]').checked;
+
+            if (!isLocked) {
+                const hiddenFields = profile.querySelector('div');
+                if (e.target.textContent === 'Show more') {
+                    hiddenFields.style.display = 'block';
+                    e.target.textContent = 'Hide it';
+                } else {
+                    hiddenFields.style.display = 'none';
+                    e.target.textContent = 'Show more';
+                }
+            }
+        });
+    });
+}
