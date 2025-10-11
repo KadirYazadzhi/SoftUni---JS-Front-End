1255d7e2054a249913c88e24f1ede68ec37c2f30
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-11 05:53:31 +0300

    Create 02. Pascal or Camel Case.js

---
+++ Exercise - DOM Introduction/02. Pascal or Camel Case.js
@@ -0,0 +1,21 @@
+function solve() {
+    const text = document.getElementById('text').value.toLowerCase();
+    const namingConvention = document.getElementById('naming-convention').value;
+    let result = '';
+
+    if (namingConvention === 'Camel Case') {
+        result = text.split(' ').map((word, index) => {
+            if (index === 0) {
+                return word;
+            } else {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            }
+        }).join('');
+    } else if (namingConvention === 'Pascal Case') {
+        result = text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+    } else {
+        result = 'Error!';
+    }
+
+    document.getElementById('result').textContent = result;
+}
