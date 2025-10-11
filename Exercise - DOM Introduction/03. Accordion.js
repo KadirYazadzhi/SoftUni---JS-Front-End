622569411f032d74539eff4c41bdb4edc9e05c6a
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-11 05:53:34 +0300

    Create 03. Accordion.js

---
+++ Exercise - DOM Introduction/03. Accordion.js
@@ -0,0 +1,12 @@
+function toggle() {
+    const extraDiv = document.getElementById('extra');
+    const button = document.getElementsByClassName('button')[0];
+
+    if (button.textContent === 'More') {
+        extraDiv.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        extraDiv.style.display = 'none';
+        button.textContent = 'More';
+    }
+}
