7cc516245a9896385cccdb4abcfc1523f4a5d339
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-13 05:54:46 +0300

    Create 03. Characters in Range.js

---
+++ Exercise - Functions and Statements /03. Characters in Range.js
@@ -0,0 +1,14 @@
+function solve(char1, char2) {
+    const code1 = char1.charCodeAt(0);
+    const code2 = char2.charCodeAt(0);
+
+    const start = Math.min(code1, code2);
+    const end = Math.max(code1, code2);
+
+    let result = '';
+    for (let i = start + 1; i < end; i++) {
+        result += String.fromCharCode(i) + ' ';
+    }
+
+    console.log(result.trim());
+}
