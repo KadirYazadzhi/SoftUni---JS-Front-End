cf1aca1791c7f3c65ba0b9e319a14a6702ef93ce
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-13 05:54:37 +0300

    Create 01. Smallest of Three Numbers.solution.js

---
+++ Exercise - Functions and Statements /01. Smallest of Three Numbers.solution.js
@@ -0,0 +1,7 @@
+function solveSolution(a, b, c) {
+    console.log(Math.min(a, b, c));
+}
+
+// Example usage:
+// solveSolution(2, 5, 3); // Output: 2
+// solveSolution(600, 342, 123); // Output: 123
