d8a35a45e1e451dd2658bc977db996a5347db5b3
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-14 05:55:34 +0300

    Create 08. Perfect Number.js

---
+++ Exercise - Functions and Statements /08. Perfect Number.js
@@ -0,0 +1,22 @@
+function solve(number) {
+    if (number <= 0) {
+        console.log("It's not so perfect.");
+        return;
+    }
+
+    let sumOfDivisors = 1;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            sumOfDivisors += i;
+            if (i !== number / i) {
+                sumOfDivisors += number / i;
+            }
+        }
+    }
+
+    if (sumOfDivisors === number) {
+        console.log("We have a perfect number!");
+    } else {
+        console.log("It's not so perfect.");
+    }
+}
