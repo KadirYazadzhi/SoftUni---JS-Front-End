f4bbca1e45a6359586cc6d6252fe097a72dfbbba
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-15 05:56:18 +0300

    Create 03. List of Names.solution.js

---
+++ Exercise - JS Arrays & Strings/03. List of Names.solution.js
@@ -0,0 +1,9 @@
+function solveSolution(names) {
+    names.sort((a, b) => a.localeCompare(b));
+    for (let i = 0; i < names.length; i++) {
+        console.log(`${i + 1}.${names[i]}`);
+    }
+}
+
+// Example usage:
+// solveSolution(["John", "Bob", "Christina", "Ema"]); // Output: 1.Bob, 2.Christina, 3.Ema, 4.John
