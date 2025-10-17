855930ac1992d2e447ecc7e65fee74a8605c5b2e
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-17 05:58:00 +0300

    Create 07. Chars to String.solution.js

---
+++ Exercise - JS Syntax Fundamentals/07. Chars to String.solution.js
@@ -0,0 +1,7 @@
+function solveSolution(char1, char2, char3) {
+    console.log(`${char1}${char2}${char3}`);
+}
+
+// Example usage:
+// solveSolution('a', 'b', 'c'); // Output: abc
+// solveSolution('%', '2', 'o'); // Output: %2o
