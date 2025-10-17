7ad14383ffc3c5ec2912656b9a41eeaeeff3b707
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-17 05:57:18 +0300

    Create 02. Vacation.solution.js

---
+++ Exercise - JS Syntax Fundamentals/02. Vacation.solution.js
@@ -0,0 +1,45 @@
+function solveSolution(count, type, day) {
+    let price = 0;
+    switch (day) {
+        case "Friday":
+            if (type === "Students") {
+                price = 8.45;
+            } else if (type === "Business") {
+                price = 10.90;
+            } else if (type === "Regular") {
+                price = 15;
+            }
+            break;
+        case "Saturday":
+            if (type === "Students") {
+                price = 9.80;
+            } else if (type === "Business") {
+                price = 15.60;
+            } else if (type === "Regular") {
+                price = 20;
+            }
+            break;
+        case "Sunday":
+            if (type === "Students") {
+                price = 10.46;
+            } else if (type === "Business") {
+                price = 16;
+            } else if (type === "Regular") {
+                price = 22.50;
+            }
+            break;
+    }
+    let totalPrice = price * count;
+    if (type === "Students" && count >= 30) {
+        totalPrice *= 0.85;
+    } else if (type === "Business" && count >= 100) {
+        totalPrice -= 10 * price;
+    } else if (type === "Regular" && count >= 10 && count <= 20) {
+        totalPrice *= 0.95;
+    }
+    console.log(`Total price: ${totalPrice.toFixed(2)}`);
+}
+
+// Example usage:
+// solveSolution(30, "Students", "Sunday"); // Output: Total price: 266.73
+// solveSolution(40, "Regular", "Saturday"); // Output: Total price: 800.00
