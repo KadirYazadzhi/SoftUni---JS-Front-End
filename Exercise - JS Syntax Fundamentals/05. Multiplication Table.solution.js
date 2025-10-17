09323615debb89dc42a3c66bd341264ad25f958d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-17 05:57:47 +0300

    Create 05. Multiplication Table.solution.js

---
+++ Exercise - JS Syntax Fundamentals/05. Multiplication Table.solution.js
@@ -0,0 +1,9 @@
+function solveSolution(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+// Example usage:
+// solveSolution(5); // Output: 5 X 1 = 5, ..., 5 X 10 = 50
+// solveSolution(2); // Output: 2 X 1 = 2, ..., 2 X 10 = 20
