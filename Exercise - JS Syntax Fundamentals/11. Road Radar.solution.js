14635f7388d1daac711a708bdd971bc18c81a8b1
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-18 05:58:28 +0300

    Create 11. Road Radar.solution.js

---
+++ Exercise - JS Syntax Fundamentals/11. Road Radar.solution.js
@@ -0,0 +1,36 @@
+function solveSolution(speed, area) {
+    let limit = 0;
+    switch (area) {
+        case "motorway":
+            limit = 130;
+            break;
+        case "interstate":
+            limit = 90;
+            break;
+        case "city":
+            limit = 50;
+            break;
+        case "residential":
+            limit = 20;
+            break;
+    }
+    let difference = speed - limit;
+    if (difference <= 0) {
+        console.log(`Driving ${speed} km/h in a ${limit} zone`);
+    } else {
+        let status = "";
+        if (difference <= 20) {
+            status = "speeding";
+        } else if (difference <= 40) {
+            status = "excessive speeding";
+        } else {
+            status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`);
+    }
+}
+
+// Example usage:
+// solveSolution(40, 'city'); // Output: Driving 40 km/h in a 50 zone
+// solveSolution(21, 'residential'); // Output: The speed is 1 km/h faster than the allowed speed of 20 - speeding
+// solveSolution(120, 'interstate'); // Output: The speed is 30 km/h faster than the allowed speed of 90 - excessive speeding
