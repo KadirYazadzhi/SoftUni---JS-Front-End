47f77c99ed49c96d53a59b5ec9f89e873b3734d6
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-18 05:58:14 +0300

    Create 08. Reversed Chars.solution.js

---
+++ Exercise - JS Syntax Fundamentals/08. Reversed Chars.solution.js
@@ -0,0 +1,7 @@
+function solveSolution(char1, char2, char3) {
+    console.log(`${char3} ${char2} ${char1}`);
+}
+
+// Example usage:
+// solveSolution('A', 'B', 'C'); // Output: C B A
+// solveSolution('1', 'L', 'a'); // Output: a L 1
