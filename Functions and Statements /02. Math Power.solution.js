b8dfff6ef6f9936324a88827a86aa15bd109f0ed
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-20 06:00:12 +0300

    Create 02. Math Power.solution.js

---
+++ Functions and Statements /02. Math Power.solution.js
@@ -0,0 +1,7 @@
+function solveSolution(number, power) {
+    console.log(Math.pow(number, power));
+}
+
+// Example usage:
+// solveSolution(2, 8); // Output: 256
+// solveSolution(3, 4); // Output: 81
