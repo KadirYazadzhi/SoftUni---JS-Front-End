5fb53bc9e011cdbe7606739f4238fb4f5c79dbd3
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-21 06:00:30 +0300

    Create 05. Simple Calculator.js

---
+++ Functions and Statements /05. Simple Calculator.js
@@ -0,0 +1,23 @@
+function solve(numOne, numTwo, operator) {
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => a / b;
+    const add = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+
+    let result = 0;
+    switch (operator) {
+        case "multiply":
+            result = multiply(numOne, numTwo);
+            break;
+        case "divide":
+            result = divide(numOne, numTwo);
+            break;
+        case "add":
+            result = add(numOne, numTwo);
+            break;
+        case "subtract":
+            result = subtract(numOne, numTwo);
+            break;
+    }
+    console.log(result);
+}
