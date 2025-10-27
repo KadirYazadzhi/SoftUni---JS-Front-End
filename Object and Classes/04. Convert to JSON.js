6cc6a544c52a9f21a4786172858b345ece355151
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-10-27 06:02:32 +0200

    Create 04. Convert to JSON.js

---
+++ Object and Classes/04. Convert to JSON.js
@@ -0,0 +1,8 @@
+function solve(name, lastName, hairColor) {
+    const obj = {
+        name: name,
+        lastName: lastName,
+        hairColor: hairColor
+    };
+    console.log(JSON.stringify(obj));
+}
